8846d71107ab1375dbcd0c7a4ed9681d4f90b035
NktGrb <130592514+NktGrb@users.noreply.github.com>
2023-07-09 18:46:25 +0700

    home work js

---
+++ 4hw.js
@@ -0,0 +1,15 @@
+let userAge = 36;
+let userInfo = "Фрилансер ${userAge}";
+
+let userHeight = 145 / 0;
+//Вернется значение NaN
+console.log(userHeight);
+
+let userName;
+//Вернется тип данных Null
+console.log(typeof userName);
+
+let userSize = "45" / "8"
+//Вернется тип данных Number
+console.log(typeof userSize)
+
